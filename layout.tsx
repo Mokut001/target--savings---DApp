736545fbfd3c842c04df14224d31d7836c67d644
Mokut001 <mokutoton01@gmail.com>
2026-02-06 01:11:18 +0100

    Delete layout.tsx

--- layout.tsx
+++
@@ -1,7 +0,0 @@
-import { MeshProvider } from '@meshsdk/react';
-import './globals.css';
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en"><body className="bg-[#0f172a]"><MeshProvider>{children}</MeshProvider></body></html>
-  );
-}
